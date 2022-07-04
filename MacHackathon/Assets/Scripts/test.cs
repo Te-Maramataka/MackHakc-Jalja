f63957a37f9c09086e969106651901d53e45690d
Arnav-Shr <arnv.shr@gmail.com>
2022-07-04 12:52:46 +1200

    Update test
    test both at same time

--- MacHackathon/Assets/Scripts/test.cs
+++ MacHackathon/Assets/Scripts/test.cs
@@ -7,12 +7,12 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Penis");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("NUTZ");
     }
 }
