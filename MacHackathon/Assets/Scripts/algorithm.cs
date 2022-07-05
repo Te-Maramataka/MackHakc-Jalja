1e2f21b3e3e58edbb8916d456ee0c6c9cef0b0e1
j <jerrysun.nz@gmail.com>
2022-07-05 14:36:14 +1200

    Created script for func

---
+++ MacHackathon/Assets/Scripts/algorithm.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public string MatarikiDay()
+    {
+        return "error";
+    }
+
+}
