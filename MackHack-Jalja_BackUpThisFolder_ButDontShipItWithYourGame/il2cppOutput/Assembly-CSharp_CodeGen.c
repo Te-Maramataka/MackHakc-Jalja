c355489e46aecb07794d77756d2011d4fabb737e
Arnav-Shr <arnv.shr@gmail.com>
2022-07-27 22:32:59 +1200

    Final Submit / maybe jsut a readmeChange

---
+++ MackHack-Jalja_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,257 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ButtonShowHide::Start()
+extern void ButtonShowHide_Start_m8EC75B7EC9BE189282C9F0279DA8C9CA32A6EF15 (void);
+// 0x00000002 System.Void ButtonShowHide::OnTriggerEnter(UnityEngine.Collider)
+extern void ButtonShowHide_OnTriggerEnter_m1D1941DBCCD8CEB9DD5E6D732B86C56F89328004 (void);
+// 0x00000003 System.Void ButtonShowHide::OnTriggerExit(UnityEngine.Collider)
+extern void ButtonShowHide_OnTriggerExit_m573D8DD23DC741E808AA49F167CCDAAB0F0F69DA (void);
+// 0x00000004 System.Void ButtonShowHide::.ctor()
+extern void ButtonShowHide__ctor_m74C517550A0CE39678D579E42CDEE8313DD5E9BA (void);
+// 0x00000005 System.Void ExitToMenu::Scene1()
+extern void ExitToMenu_Scene1_mF2BBB9BA35C36C5ABB170EF1CCB7F63E6EE138D6 (void);
+// 0x00000006 System.Void ExitToMenu::.ctor()
+extern void ExitToMenu__ctor_m4FE3DFCE973129C9C9C256A0AFC5B3DD99B19FC8 (void);
+// 0x00000007 System.Void FPController::Start()
+extern void FPController_Start_m2D9220D0F301F68954F89E2308E4A27DEF50A720 (void);
+// 0x00000008 System.Void FPController::Update()
+extern void FPController_Update_mAFDA64FAE231A35CDF3A3C41285B5AC95342878E (void);
+// 0x00000009 System.Void FPController::GetTouchInput()
+extern void FPController_GetTouchInput_m2F7C0B1EBD9A509DC6E32B20BF9AAD976686B4F3 (void);
+// 0x0000000A System.Void FPController::LookAround()
+extern void FPController_LookAround_m41BFD9C6908DCB48996C92FCEE37CA618A739EA2 (void);
+// 0x0000000B System.Void FPController::Move()
+extern void FPController_Move_mA468453073B7706FABC4C6FE3790661C13DF4097 (void);
+// 0x0000000C System.Void FPController::.ctor()
+extern void FPController__ctor_mF6191E82B3FB94C4F7BA69DDF0057A07340AEA35 (void);
+// 0x0000000D System.Void PlayerBehave::Start()
+extern void PlayerBehave_Start_m83EF9971F1964A6E881D29E3BB43D3D706258BBF (void);
+// 0x0000000E System.Void PlayerBehave::FixedUpdate()
+extern void PlayerBehave_FixedUpdate_m19A1CDE857E2622DE104D709889EBED1973C2E22 (void);
+// 0x0000000F System.Void PlayerBehave::.ctor()
+extern void PlayerBehave__ctor_m7EC0DCED2D5083E72DE7DBFCA23D8CBDB8C5AA5A (void);
+// 0x00000010 System.Void PlayerView::Start()
+extern void PlayerView_Start_m59C8148D9B14E475032908499028156D9F22CCF1 (void);
+// 0x00000011 System.Void PlayerView::FixedUpdate()
+extern void PlayerView_FixedUpdate_mD735175E0B36CB13E4BC67CEF98D0A8AF47E3260 (void);
+// 0x00000012 System.Void PlayerView::.ctor()
+extern void PlayerView__ctor_m55E6ABBE6C11BC883CFB7F8D3CFBF03341A3DCFB (void);
+// 0x00000013 System.Void SkyMove::Update()
+extern void SkyMove_Update_m88C18E86ECC81664D89CDDF17ADD2D07E1187E14 (void);
+// 0x00000014 System.Void SkyMove::.ctor()
+extern void SkyMove__ctor_m69AC09549912B35F8E9E67BCEBA03F1954C4D838 (void);
+// 0x00000015 System.Void algorithm::StoreYear()
+extern void algorithm_StoreYear_m92606EB955A11072168FD3CFB870B742939CBDE2 (void);
+// 0x00000016 System.DateTime algorithm::MatarikiDay(System.Int32)
+extern void algorithm_MatarikiDay_mE2B2454A1D85A4C7AC1EAF40372CB8F7F2CC57CF (void);
+// 0x00000017 System.Boolean algorithm::IsCorrectMoonPhase(System.DateTime)
+extern void algorithm_IsCorrectMoonPhase_m7C8DB7992BEF214C656A064E6A509B63AF110F78 (void);
+// 0x00000018 System.DateTime algorithm::FirstFriday(System.DateTime)
+extern void algorithm_FirstFriday_m58390DC68E5CFA83B2F906D408E6DFE3CAEFB355 (void);
+// 0x00000019 System.Void algorithm::.ctor()
+extern void algorithm__ctor_m303B03AF9B647808E0C4207B65A41928656BC338 (void);
+// 0x0000001A System.Void ArCursor::Start()
+extern void ArCursor_Start_mF76184F23C8F97E72D0D27B0986D874FAD6DE5DC (void);
+// 0x0000001B System.Void ArCursor::Update()
+extern void ArCursor_Update_m5AFDEB0E42AB985D89772840B87F3022575856CD (void);
+// 0x0000001C System.Void ArCursor::UpdateCursor()
+extern void ArCursor_UpdateCursor_mF27AD8D4DDF72741D7F0A7D4D98B265B0742FA77 (void);
+// 0x0000001D System.Void ArCursor::.ctor()
+extern void ArCursor__ctor_mBBCAA7E3BB2FDD360867C04409FED36F5A3D0DC0 (void);
+// 0x0000001E System.Void ClickerAudio::Start()
+extern void ClickerAudio_Start_m08BFC5DCFF5B89C0A75700DF52503A995698122E (void);
+// 0x0000001F System.Void ClickerAudio::Update()
+extern void ClickerAudio_Update_m6513D84588DA5262593959B14BAE03EE45E953FD (void);
+// 0x00000020 System.Void ClickerAudio::.ctor()
+extern void ClickerAudio__ctor_m71A2124E81600BE07E160117A1769BD9FA294007 (void);
+// 0x00000021 System.Void InputCalc::Start()
+extern void InputCalc_Start_m790C8EE546C94A7BBEC39AC1655F1480FC6206E7 (void);
+// 0x00000022 System.Void InputCalc::Update()
+extern void InputCalc_Update_m06731029D2FD33E1E54CED82C8C937D6525B4CB4 (void);
+// 0x00000023 System.Void InputCalc::.ctor()
+extern void InputCalc__ctor_m965C6353D375B78D1CA2D8703A5170F86115EA62 (void);
+// 0x00000024 System.Void MainMenu::Start()
+extern void MainMenu_Start_m1729BDE6D096D9F4C92DBE72B392BA89E9A9ECAD (void);
+// 0x00000025 System.Void MainMenu::Update()
+extern void MainMenu_Update_m6D9E8EB1A42CC68CFAA865B9CF18FAEB81595C5C (void);
+// 0x00000026 System.Void MainMenu::.ctor()
+extern void MainMenu__ctor_m8209CEC1D907C87A96D777961F4D0536E6E948DD (void);
+// 0x00000027 System.Void OptionMenu::setVolume(System.Single)
+extern void OptionMenu_setVolume_m3864878E5560F9E73FB6743E8A0724DF41418A81 (void);
+// 0x00000028 System.Void OptionMenu::setSFXVolume(System.Single)
+extern void OptionMenu_setSFXVolume_m51E37200507065778885F8F2E48558CDC76276B8 (void);
+// 0x00000029 System.Void OptionMenu::.ctor()
+extern void OptionMenu__ctor_m237393215444C8302245F88E3210D23355B677C6 (void);
+// 0x0000002A System.Void Random::Start()
+extern void Random_Start_mEA4D64777F45C906E24E059AA549C035EA49D355 (void);
+// 0x0000002B System.Void Random::Update()
+extern void Random_Update_mF84906E16DEF869A4AA116BF674F5B5F28756808 (void);
+// 0x0000002C System.Void Random::.ctor()
+extern void Random__ctor_m7BAA71A5710B0E964AF2DAFF38B1F099676F2E05 (void);
+// 0x0000002D System.Void ScenesManager::Loader(System.String)
+extern void ScenesManager_Loader_mA438E7CD9053BFB8EAC061F46BA833400FB3EFB0 (void);
+// 0x0000002E System.Void ScenesManager::.ctor()
+extern void ScenesManager__ctor_m9012BEC3C25B1A916374854A9A99DC29C1D7A5D8 (void);
+// 0x0000002F System.Void Scroller::Update()
+extern void Scroller_Update_m30B68B087B963C89D4E284E032A980DABDAEB753 (void);
+// 0x00000030 System.Void Scroller::.ctor()
+extern void Scroller__ctor_m248C79CD435F4A786C189D2A8E1545E9038E647F (void);
+// 0x00000031 System.Void SoundManager::Start()
+extern void SoundManager_Start_m114BCC9E38EB73FB08543ABE6B95FFA5756D7AF7 (void);
+// 0x00000032 System.Void SoundManager::OnMusicButtonPress()
+extern void SoundManager_OnMusicButtonPress_m3B932B928CFC63E5488C687DCBC1B34C34B517AA (void);
+// 0x00000033 System.Void SoundManager::UpdateMusicButtonImage()
+extern void SoundManager_UpdateMusicButtonImage_mF4C0D3C4710809BEF75ADB45F9218C016B797D6B (void);
+// 0x00000034 System.Void SoundManager::OnSFXButtonPress()
+extern void SoundManager_OnSFXButtonPress_mC2F3004ED993FC2360C395A74B7AEDFE0E534632 (void);
+// 0x00000035 System.Void SoundManager::UpdateSFXButtonImage()
+extern void SoundManager_UpdateSFXButtonImage_m0196D39AD86C2EFFA0F41B0DD2FF3734E9A7A900 (void);
+// 0x00000036 System.Void SoundManager::.ctor()
+extern void SoundManager__ctor_m27816732AF730AF6BEDE4A67ABC9D1A094777213 (void);
+static Il2CppMethodPointer s_methodPointers[54] = 
+{
+	ButtonShowHide_Start_m8EC75B7EC9BE189282C9F0279DA8C9CA32A6EF15,
+	ButtonShowHide_OnTriggerEnter_m1D1941DBCCD8CEB9DD5E6D732B86C56F89328004,
+	ButtonShowHide_OnTriggerExit_m573D8DD23DC741E808AA49F167CCDAAB0F0F69DA,
+	ButtonShowHide__ctor_m74C517550A0CE39678D579E42CDEE8313DD5E9BA,
+	ExitToMenu_Scene1_mF2BBB9BA35C36C5ABB170EF1CCB7F63E6EE138D6,
+	ExitToMenu__ctor_m4FE3DFCE973129C9C9C256A0AFC5B3DD99B19FC8,
+	FPController_Start_m2D9220D0F301F68954F89E2308E4A27DEF50A720,
+	FPController_Update_mAFDA64FAE231A35CDF3A3C41285B5AC95342878E,
+	FPController_GetTouchInput_m2F7C0B1EBD9A509DC6E32B20BF9AAD976686B4F3,
+	FPController_LookAround_m41BFD9C6908DCB48996C92FCEE37CA618A739EA2,
+	FPController_Move_mA468453073B7706FABC4C6FE3790661C13DF4097,
+	FPController__ctor_mF6191E82B3FB94C4F7BA69DDF0057A07340AEA35,
+	PlayerBehave_Start_m83EF9971F1964A6E881D29E3BB43D3D706258BBF,
+	PlayerBehave_FixedUpdate_m19A1CDE857E2622DE104D709889EBED1973C2E22,
+	PlayerBehave__ctor_m7EC0DCED2D5083E72DE7DBFCA23D8CBDB8C5AA5A,
+	PlayerView_Start_m59C8148D9B14E475032908499028156D9F22CCF1,
+	PlayerView_FixedUpdate_mD735175E0B36CB13E4BC67CEF98D0A8AF47E3260,
+	PlayerView__ctor_m55E6ABBE6C11BC883CFB7F8D3CFBF03341A3DCFB,
+	SkyMove_Update_m88C18E86ECC81664D89CDDF17ADD2D07E1187E14,
+	SkyMove__ctor_m69AC09549912B35F8E9E67BCEBA03F1954C4D838,
+	algorithm_StoreYear_m92606EB955A11072168FD3CFB870B742939CBDE2,
+	algorithm_MatarikiDay_mE2B2454A1D85A4C7AC1EAF40372CB8F7F2CC57CF,
+	algorithm_IsCorrectMoonPhase_m7C8DB7992BEF214C656A064E6A509B63AF110F78,
+	algorithm_FirstFriday_m58390DC68E5CFA83B2F906D408E6DFE3CAEFB355,
+	algorithm__ctor_m303B03AF9B647808E0C4207B65A41928656BC338,
+	ArCursor_Start_mF76184F23C8F97E72D0D27B0986D874FAD6DE5DC,
+	ArCursor_Update_m5AFDEB0E42AB985D89772840B87F3022575856CD,
+	ArCursor_UpdateCursor_mF27AD8D4DDF72741D7F0A7D4D98B265B0742FA77,
+	ArCursor__ctor_mBBCAA7E3BB2FDD360867C04409FED36F5A3D0DC0,
+	ClickerAudio_Start_m08BFC5DCFF5B89C0A75700DF52503A995698122E,
+	ClickerAudio_Update_m6513D84588DA5262593959B14BAE03EE45E953FD,
+	ClickerAudio__ctor_m71A2124E81600BE07E160117A1769BD9FA294007,
+	InputCalc_Start_m790C8EE546C94A7BBEC39AC1655F1480FC6206E7,
+	InputCalc_Update_m06731029D2FD33E1E54CED82C8C937D6525B4CB4,
+	InputCalc__ctor_m965C6353D375B78D1CA2D8703A5170F86115EA62,
+	MainMenu_Start_m1729BDE6D096D9F4C92DBE72B392BA89E9A9ECAD,
+	MainMenu_Update_m6D9E8EB1A42CC68CFAA865B9CF18FAEB81595C5C,
+	MainMenu__ctor_m8209CEC1D907C87A96D777961F4D0536E6E948DD,
+	OptionMenu_setVolume_m3864878E5560F9E73FB6743E8A0724DF41418A81,
+	OptionMenu_setSFXVolume_m51E37200507065778885F8F2E48558CDC76276B8,
+	OptionMenu__ctor_m237393215444C8302245F88E3210D23355B677C6,
+	Random_Start_mEA4D64777F45C906E24E059AA549C035EA49D355,
+	Random_Update_mF84906E16DEF869A4AA116BF674F5B5F28756808,
+	Random__ctor_m7BAA71A5710B0E964AF2DAFF38B1F099676F2E05,
+	ScenesManager_Loader_mA438E7CD9053BFB8EAC061F46BA833400FB3EFB0,
+	ScenesManager__ctor_m9012BEC3C25B1A916374854A9A99DC29C1D7A5D8,
+	Scroller_Update_m30B68B087B963C89D4E284E032A980DABDAEB753,
+	Scroller__ctor_m248C79CD435F4A786C189D2A8E1545E9038E647F,
+	SoundManager_Start_m114BCC9E38EB73FB08543ABE6B95FFA5756D7AF7,
+	SoundManager_OnMusicButtonPress_m3B932B928CFC63E5488C687DCBC1B34C34B517AA,
+	SoundManager_UpdateMusicButtonImage_mF4C0D3C4710809BEF75ADB45F9218C016B797D6B,
+	SoundManager_OnSFXButtonPress_mC2F3004ED993FC2360C395A74B7AEDFE0E534632,
+	SoundManager_UpdateSFXButtonImage_m0196D39AD86C2EFFA0F41B0DD2FF3734E9A7A900,
+	SoundManager__ctor_m27816732AF730AF6BEDE4A67ABC9D1A094777213,
+};
+static const int32_t s_InvokerIndices[54] = 
+{
+	4618,
+	3706,
+	3706,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	6813,
+	6777,
+	6812,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	3739,
+	3739,
+	4618,
+	4618,
+	4618,
+	4618,
+	3706,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+	4618,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	54,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
--- README.md
+++ README.md
@@ -7,5 +7,8 @@ https://youtu.be/ZQ9FXcrhbmA
 How to use
 1. Open up the app through your phone 
 2. Click on ar / gallery to view them 
-3. Input year (isn't implement yet)
-4. Code can be test through using either algorithm.cs or matarikicalculator.py both using different algorithm as there is many way you can solve the problem  
+3. Input year 
+4. Code can be test through using either algorithm.cs, will have to messa bout a little bit, just create a Main/Start function depending on platform
+
+This was made mainly on a few computers, with others helping behind the scenes with code, prefabs, designing, that is why you see a slightly skewed commit page.
+Thank you for using our program, we hope you also test out the program by installing the APK!
